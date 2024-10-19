18046008aac965687b005ff9b6a8f556a96a663a
Shawn <flyflyk2004@gmail.com>
2024-10-19 20:50:40 +0800

    註冊和登入與資料庫連接

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
-from flask import Flask, redirect, render_template, request, url_for
-from flask_mail import Mail
+from flask import Flask, jsonify, redirect, render_template, request, url_for
 from flask_cors import CORS
+import sqlite3
 
 app = Flask(__name__, template_folder='app/templates', static_folder='app/static')
-
-# 初始化 Flask-Mail
-mail = Mail(app)
 
 # 啟用跨來源資源共享 (CORS)
 CORS(app)
@@ -14,28 +11,69 @@ CORS(app)
 def index():
     return render_template('index.html')
 
-
 @app.route('/signup', methods=['POST'])
 def signup():
-    # 獲取表單數據
     username = request.form['username']
     password = request.form['password']
-    email = request.form['email']
     
     # 保存帳戶資料到數據庫
+    conn = sqlite3.connect('instance/app.db')
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO users (username, password) VALUES (?, ?)', (username, password))
+    conn.commit()
+    conn.close()
+    return redirect(url_for('index')) 
 
-    return redirect(url_for('index'))  # 成功後重新導向主頁
+@app.route('/check_username', methods=['POST'])
+def check_username():
+    data = request.get_json()
+    username = data.get('username')
+
+    conn = sqlite3.connect('instance/app.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT COUNT(*) FROM users WHERE username = ?', (username,))
+    count = cursor.fetchone()[0]  # 獲取查到的第一個結果
+    conn.close()
+
+    return jsonify({'exists': count > 0})
+
+@app.route('/check_login', methods=['POST'])
+def check_login():
+    data = request.get_json()  
+    username = data.get('username')
+    password = data.get('password')
+
+    # 用戶驗證
+    conn = sqlite3.connect('instance/app.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT password FROM users WHERE username = ?', (username,))
+    result = cursor.fetchone()
+    if result is None:
+        return jsonify({'success': False, 'errorCode': 1, 'message': '用戶名不存在'})
+    elif result[0] != password:
+        return jsonify({'success': False, 'errorCode': 2, 'message': '密碼不正確'})
+    else :
+        return jsonify({'success': True, 'errorCode': 0, 'message': '登入成功'})
 
 @app.route('/login', methods=['POST'])
 def login():
-    # 獲取表單數據
-    usernamee = request.form['username']
-    password = request.form['password']
+    return redirect(url_for('index'))
 
-    # 用戶驗證
-
-    return redirect(url_for('index'))  # 成功後重新導向主頁
+def create_users_table():
+    conn = sqlite3.connect('instance/app.db')
+    cursor = conn.cursor()
+    
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT NOT NULL,
+            password TEXT NOT NULL
+        )
+    ''')
+    
+    conn.commit()
+    conn.close()
 
 if __name__ == '__main__':
-    # 啟動應用
+    create_users_table()
     app.run(debug=True)
