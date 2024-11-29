b26f1f40fd37b1afb5e7e10018f68890bb04b601
CHUCHENGHUNG <edison05175803@gmail.com>
2024-11-29 22:41:56 +0800

    2個小時

--- app.py
+++ app.py
@@ -25,4 +25,5 @@ def index():
 if __name__ == '__main__':
     dbHandler.initUsers()
     dbHandler.initRooms()
-    app.run(debug=True, port=8443, ssl_context=('cert.pem', 'key.pem'))
+    app.run(host='0.0.0.0',debug=True, port=8443, ssl_context=('cert.pem', 'key.pem'))
+   
